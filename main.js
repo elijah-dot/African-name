b278b6bd448a33e98091482bc3905e5b61b22896
Moringa School <wangu.mwangi@student.moringaschool.com>
2022-03-05 11:17:11 +0300

    feat:started on js.file

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 //validation for all inputs
+let form = document.getElementById("form");
 let inputToday = document.getElementById("name");
 let inputGender = document.getElementById("gender");
 let inputDate = document.getElementById("birth");
@@ -8,22 +9,3 @@ let today_error = document.getElementById("name_error");
 let gender_error = document.getElementById("gender_error");
 let date_error = document.getElementById("date_error");
 //validation of button submit
-
-function mine(){
-    if(inputToday.value === "" || inputToday.value === null){
-        inputToday.style.border = "red";
-        today_error.style.display = "block";
-        inputToday.focus
-        return false;
-    }
-    if(inputGender.value === "" || inputGender.value === null){
-        inputGender.style.border = "red";
-        gender_error.style.display = "block";
-        inputGender.focus
-        return false;}
-    if(inputDate.value === "" || inputDate.value === null){
-        inputGender.style.border = "red";
-        gender_error.style.display = "block";
-        inputGender.focus
-        return false;}
-}  
